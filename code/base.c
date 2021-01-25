0a5db598c526c09b0487c7d11b5a4dd284f5eb22
Andrea Minetti <andrea.minetti@wavein.ch>
2021-01-25 18:42:21 +0100

    +Alberto meg.inout

--- code/base.c
+++ code/base.c
@@ -1,38 +1,19 @@
-/**
-   CODICE BASE
-   Give the same name to the project, the max object and the .c file; prepend "_" to the struct
-*/
 
-// standard Max LIBRARY to include, always required
 #include "ext.h"
-
-// required for new style Max object
 #include "ext_obex.h"
 
-/* 1
-====================================================================
-DEFINE A STRUCTURE
-Insert here the global variables of your object, including the object itself.
-The outlets of the object have to be included here as pointers.
-====================================================================
-*/
-
-typedef struct _base
+typedef struct _inout
 {
-    // the object itself (always need, first);
-    // t_object is the type for the max objects
     t_object s_ob;
     
-    // global variables you need
-    long s_value0;
-    long s_value1;
-    long sum;
-    
     // outlets (as void pointers)
-    void *m_outlet1;
-    void *m_outlet2;
+    void *outlet1_int;
+    void *outlet2_double;
+    void *outlet3_string;
+    void *outlet4_list;
+    void *outlet5_bang;
 
-} t_base; // with this you instantiate the structure; this is the name of the instance of the structure _base
+} t_inout; 
 
 /* 2
 ====================================================================
@@ -42,70 +23,49 @@ Definition of the methods of your max object
 */
 
 // constructor
-void *base_new();
+void *inout_new();
 
 // metodi che rispondono ai messaggi mandati all'oggetto attraverso gli inlets
-void inout_int(t_base *x, long n); // inlet left-most
-void inout_in1(t_base *x, long n); // inlet right-most
-void inout_in2(t_base *x, long n); // in questo caso il centrale
+// int/bang - list - string - double
+// +---o-------o-------o--------o---+
 
-// bang
-void inout_bang(t_base *x);
-
-// metodo che usa A_GIMME
-// t_symbol è il "selettore"
-// argc è il numero di elementi nell'argv (che è l'array di atom)
-void myobject_printargs(t_base *x, t_symbol *s, long argc, t_atom *argv);
-
-/* 3
-    ====================================================================
-    GLOBAL CLASS POINTER VARIABLE
-    Definisco una variabile puntatore di tipo t_class
-    ====================================================================
-*/
+void inlet_left_int(t_inout *x, long n); // inlet left-most
+void inlet_left_bang(t_inout *x);
+void inlet_left_float(t_inout *x, double n); // inlet right-most
+void inlet_left_string(t_inout *x, char *str); // in questo caso il centrale
+void inlet_left_list(t_inout *x, t_symbol *s, long l, t_atom *av); // in questo caso il centrale
 
 static t_class *inout_class;
-
-/* 4
-    ====================================================================
-    INITIALIZATION ROUTINE (ext_main)
-    Funzione che viene chiamata da Max quando creo un nuovo oggetto
-    ====================================================================
-*/
 
 void ext_main(void *r)
 {
     t_class *c;
     
-    // creo la classe ("inlets" è il nome dell'oggetto in Max)
-    // 1. nome; 2. the new instance routine (chiamo il costruttore); 3. una free function;
-    // 4. la size della struttura; 5. un argomento non più usato;
-    // 6. descrizione degli argomenti da inserire quando creimo l'oggetto
-    c = class_new("inout", (method)base_new, (method)NULL,  (long)sizeof(t_base), 0L , A_GIMME, 0);
+    c = class_new("meg.inout", (method)inout_new, (method)NULL,  (long)sizeof(t_inout), 0L, 0);
     
     // aggiungo alla classe i metodi
     // associamo un metodo in base al tipo di messaggio che l'oggetto riceve e in base all'inlet
     // i selettori dei messaggi non sono stringhe, ma speciali strutture dati chiamati symbol (t_symbol)
     // ogni simbolo in max è unico e contiene una stringa e un valore. Il dato del messaggio è passato come array di atoms (t_atmo), una struttura che può contenere interi, float, o anche pointers, identificati da un tag.
     
-    
     // i tipi possibili sono: A_LONG - A_FLOAT - A_SYM - A_GIMME; quest'ultimo passa la lista ti atoms
     // i TIPED METHOD sono quelli in cui Max controlla il tipo di ogni atom in un messaggio per controllare la consistenza con ciò che si aspetta di ottenere
-    class_addmethod(c, (method)inout_int, "int", A_LONG, 0); // left most inlet - A_LONG dice che la funzione accetta un intero long e lo zero successivo termina la lista degli argomenti.
-    
-    class_addmethod(c, (method)inout_in1, "in1", A_LONG, 0); // right most inlet
-    class_addmethod(c, (method)inout_in2, "in2", A_LONG, 0); // ...
 
-    class_addmethod(c, (method)inout_bang, "bang", 0);
-    
-    class_addmethod(c, (method)myobject_printargs, "printargs", A_GIMME, 0);
+    /* 
+    name : C-string defining the message (message selector)
+...	One or more integers specifying the arguments to the message, in the standard Max type list format (see Chapter 3 of the Writing Externals in Max document for more information).
+    */
+    class_addmethod(c, (method)inlet_left_int, "int", A_LONG, 0); // left most inlet - A_LONG dice che la funzione accetta un intero long e lo zero successivo termina la lista degli argomenti.
+    class_addmethod(c, (method)inlet_left_bang, "bang", 0);
+    class_addmethod(c, (method)inlet_left_float, "float", A_FLOAT, 0); // right most inlet
+    class_addmethod(c, (method)inlet_left_string, "sym", A_SYM, 0); // ...
+    class_addmethod(c, (method)inlet_left_list, "list", A_GIMME, 0); // ...
     
     // aggiungo la classe al name space CLASS_BOX,
     // in modo che questa classe possa essere trovata dall'utente quando crea un oggetto in Max.
     class_register(CLASS_BOX, c);
     
     inout_class = c;
-    
 }
 
 /* 5
@@ -115,20 +75,22 @@ void ext_main(void *r)
     ====================================================================
 */
 
-void *base_new()
+void *inout_new()
 {
     // istanzio un oggetto della classe
-    t_base *x = (t_base *)object_alloc(inout_class);
+    t_inout *x = (t_inout *)object_alloc(inout_class);
     
-    // creo gli inlets
-    intin(x, 1); // right-most
-    intin(x, 2);
+    // first inlet is created by default
     
     // creo un outlet di tipo intero - right-most outlet
-    x->m_outlet1 = intout((t_object *)x); // equivale a: outlet_new((t_object *)x, "int");
-    
+    x->outlet1_int = intout((t_object *)x); // equivale a: outlet_new((t_object *)x, "int");
+    x->outlet2_double = floatout((t_object *)x); // equivale a: outlet_new((t_object *)x, "int");
+    x->outlet3_string = outlet_new((t_object *)x, "sym"); 
+    x->outlet4_list = outlet_new((t_object *)x, "list"); 
+    x->outlet5_bang = bangout((t_object *)x); 
+
     // creo un generico outlet
-    x->m_outlet2 = outlet_new((t_object *)x, NULL); // NULL indica che possiamo mandare fuori dall'outlet qualsiasi tipo di messaggio
+    //x->m_outlet2 = outlet_new((t_object *)x, NULL); // NULL indica che possiamo mandare fuori dall'outlet qualsiasi tipo di messaggio
     
     return (x);
 }
@@ -140,63 +102,77 @@ void *base_new()
     ====================================================================
 */
 
-void inout_int(t_base *x, long n) {
-    x->s_value0 = n;
+void inlet_left_int(t_inout *x, long n){
+    outlet_int(x->outlet1_int, n);
 }
 
-void inout_in1(t_base *x, long n) {
-    x->s_value1 = n;
+void inlet_left_bang(t_inout *x){
+    outlet_bang(x->outlet5_bang);
 }
 
-void inout_in2(t_base *x, long n) {
-    x->s_value2 = n;
+void inlet_left_float(t_inout *x, double n) {
+    outlet_float(x->outlet2_double,n);
+}
+void inlet_left_string(t_inout *x, char *str) {
+    t_atom string_to_send;
+    atom_setsym(&string_to_send, gensym(str));
+    outlet_anything(x->outlet3_string, gensym("sym"), 1, &string_to_send);
+}
+void inlet_left_list(t_inout *x, t_symbol *s, long l, t_atom *av) {
+    outlet_list(x->outlet4_list,s,l,av);
 }
 
-void inout_bang(t_base *x) {
-    
-    t_atom argv[3];
-    atom_setlong(argv, 43);
-    atom_setsym(argv + 1, gensym("crazy")); // gen sym è il message selector
-    atom_setfloat(argv+2, 8.3);
-    
-    post("value0 is %ld", x->s_value0); // left-most
-    post("value1 is %ld", x->s_value2); // centrale
-    post("value2 is %ld", x->s_value1); // right-most
-    
-    x->sum = x->s_value0 + x->s_value1 + x->s_value2;
-    
-    // outlet intero
-    outlet_int(x->m_outlet1, x->sum);
-    
-    // outlet generico
-    // green è la "key" alla quale attacco un array di tipo t_atom di 3 elementi
-    outlet_anything(x->m_outlet2,gensym("green"), 3, argv);
-}
 
-void myobject_printargs(t_base *x, t_symbol *s, long argc, t_atom *argv){
-    long i;
-    t_atom *ap;
+// void inlet3_list(t_inout *x, t_atom *n) {
+//     post("dbg");
+//     //x->s_value2 = n;
+// }
+
+// void inout_bang(t_base *x) {
     
-    post("message selector is %s", s->s_name);
-    post("there are %ld arguments", argc);
+//     t_atom argv[3];
+//     atom_setlong(argv, 43);
+//     atom_setsym(argv + 1, gensym("crazy")); // gen sym è il message selector
+//     atom_setfloat(argv+2, 8.3);
     
-    // increment ap each time to get to the next atom
-    for(i=0, ap=argv; i<argc; i++, ap++){
-        switch(atom_gettype(ap)){
-            case A_LONG:
-                post("%ld: %ld", i+1,atom_getlong(ap));
-                break;
-            case A_FLOAT:
-                post("%ld: %.2f", i+1,atom_getfloat(ap));
-                break;
-            case A_SYM:
-                post("%ld: %s", i+1,atom_getsym(ap)->s_name);
-                break;
-            default:
-                post("%ld: unknown atom type %ld", i+1,atom_gettype(ap));
-                break;
-        }
-    }
-}
+//     post("value0 is %ld", x->s_value0); // left-most
+//     post("value1 is %ld", x->s_value2); // centrale
+//     post("value2 is %ld", x->s_value1); // right-most
+    
+//     x->sum = x->s_value0 + x->s_value1 + x->s_value2;
+    
+//     // outlet intero
+//     outlet_int(x->m_outlet1, x->sum);
+    
+//     // outlet generico
+//     // green è la "key" alla quale attacco un array di tipo t_atom di 3 elementi
+//     outlet_anything(x->m_outlet2,gensym("green"), 3, argv);
+// }
+
+// void myobject_printargs(t_base *x, t_symbol *s, long argc, t_atom *argv){
+//     long i;
+//     t_atom *ap;
+    
+//     post("message selector is %s", s->s_name);
+//     post("there are %ld arguments", argc);
+    
+//     // increment ap each time to get to the next atom
+//     for(i=0, ap=argv; i<argc; i++, ap++){
+//         switch(atom_gettype(ap)){
+//             case A_LONG:
+//                 post("%ld: %ld", i+1,atom_getlong(ap));
+//                 break;
+//             case A_FLOAT:
+//                 post("%ld: %.2f", i+1,atom_getfloat(ap));
+//                 break;
+//             case A_SYM:
+//                 post("%ld: %s", i+1,atom_getsym(ap)->s_name);
+//                 break;
+//             default:
+//                 post("%ld: unknown atom type %ld", i+1,atom_gettype(ap));
+//                 break;
+//         }
+//     }
+// }
 
 
